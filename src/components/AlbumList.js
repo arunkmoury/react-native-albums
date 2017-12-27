3f4a7fc26a7e8af6ec034be554f75d2b13189030
arunkmoury <arunkmoury@gmail.com>
2017-12-27 23:13:43 +0530

    API request fixed and data fetched in console'

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -4,18 +4,22 @@ import { Text, View } from 'react-native';
 
 
 class AlbumList extends Component {
-  //const REQUEST_URL = 'https://ws.audioscrobbler.com/2.0/?method=chart.gettoptracks&api_key=881262b246e1d3f2abda8771b1a25fe3&format=json';
-  // componentWillMount() {
-  //   fetch('https://ws.audioscrobbler.com/2.0/?method=chart.gettoptracks&api_key=881262b246e1d3f2abda8771b1a25fe3&format=json')
-  //     .then(response => console.log(response))
-  //     .catch(function(error) {
-  //       console.log("Something happpened");
-  //     });
-    //alert("Arun");
-  //}
-
+  state = { albums: [] };
+  componentWillMount() {
+    
+    //fetch('https://ws.audioscrobbler.com/2.0/?method=chart.gettoptracks&api_key=881262b246e1d3f2abda8771b1a25fe3&format=json')
+    fetch('https://rallycoding.herokuapp.com/api/music_albums')
+      .then(response => response.json())
+      .then( data => this.setState({ albums: data }));
+      // .then((response) => response.json())
+      // .then(albums => this.setState({ albums }));
+      //.catch(function(error) {
+        //       console.log("Something happpened");
+        //     });
+    }
 
   render() {
+    console.log(this.state);
     return (
       <View>
         <Text>AlbumList</Text>
